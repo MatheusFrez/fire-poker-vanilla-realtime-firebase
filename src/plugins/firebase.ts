937562756fb55d4f797b31ba36838ee1cce61ca1
MatheusFrez <matheusfrez.nf@gmail.com>
2020-11-20 11:10:12 -0300

    Firebase configurado.

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDGm-psNe8QyQ_aSfqxIWU7n-NwmwAfap8',
+    authDomain: 'fire-poker-e61f8.firebaseapp.com',
+    databaseURL: 'https://fire-poker-e61f8.firebaseio.com',
+    projectId: 'fire-poker-e61f8',
+    storageBucket: 'fire-poker-e61f8.appspot.com',
+    messagingSenderId: '105128322524',
+    appId: '1:105128322524:web:ce4c48aa28dbd959541b19',
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({});
+}
+
+export const fireDb = firebase.firestore();

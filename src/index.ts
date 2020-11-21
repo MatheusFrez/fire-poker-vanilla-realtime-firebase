d9d96d3eedee636b25818a380408883663fef12c
MatheusFrez <matheusfrez.nf@gmail.com>
2020-11-21 10:37:06 -0300

    Criada estrutura inicial acesso firebase. (#14)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 import './register-service-worker';
+import RoomFiredbSingletonService from './services/room-firedb-service';
+import Room from './models/room';
+
+const roomService: RoomFiredbSingletonService = RoomFiredbSingletonService.getInstance();
+
+roomService.upsert({
+  finished: false,
+  identifier: 'dasiudashuadsk CHANGED 3',
+  players: [],
+  title: 'teste',
+  userStories: [],
+  id: 'dsasdasaddsa',
+});
+
+roomService.listenCollection().subscribe(
+  (room: Room) => {
+    // console.log('SALA QUE CHEGOU QUE FOI ALTERADA', room);
+  },
+);
---
+++ src/services/room-firedb-service.ts
@@ -0,0 +1,48 @@
+import Room from '../models/room';
+import { fireDb } from '../plugins/firebase';
+import { Observable } from 'rxjs';
+
+export default class RoomFiredbSingletonService {
+  private static instance: RoomFiredbSingletonService;
+  public collection: string = 'room'
+  private constructor () {}
+
+  public static getInstance () {
+    if (!this.instance) this.instance = new RoomFiredbSingletonService();
+    return this.instance;
+  }
+
+  public async findById (id: string): Promise<Room> {
+    return fireDb.collection(this.collection).doc(id).get()
+      .then(res => new Room(res as any)); // TO DO REVER ESSE as any
+  }
+
+  public async remove (id: string): Promise<void> {
+    return fireDb.collection(this.collection)
+      .doc(id)
+      .delete();
+  }
+
+  public async upsert (room: Room): Promise<Room> {
+    return fireDb.collection(this.collection)
+      .doc(room.id)
+      .set(room, { merge: true })
+      .then(() => room);
+  }
+
+  public listenCollection (): Observable<Room> {
+    try {
+      return Observable.create(
+        (observer) => {
+          fireDb.collection(this.collection)
+            .onSnapshot(querySnapshot => {
+              querySnapshot.forEach((docChanged) => {
+                observer.next(docChanged.data());
+              });
+            });
+        },
+      );
+    } catch (e) {
+    }
+  }
+}
